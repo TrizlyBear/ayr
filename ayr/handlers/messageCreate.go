f1b804904177ddd4a881a6ad6b31f3b6ce209e9d
TrizlyBear <tristanschmidt2004@gmail.com>
2021-03-26 10:14:24 +0100

    Test

--- ayr/handlers/messageCreate.go
+++ ayr/handlers/messageCreate.go
@@ -4,7 +4,7 @@ import "github.com/bwmarrin/discordgo"
 
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate)  {
 	if !m.Author.Bot && m.Content == "Ping!"{
-		s.ChannelMessageSend(m.ChannelID, "Pong! :ping_pong: "+s.HeartbeatLatency().String())
+		s.ChannelMessageSend(m.ChannelID, "test! :ping_pong: "+s.HeartbeatLatency().String())
 	}
 
 }
