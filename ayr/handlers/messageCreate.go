02f2427465c6cd176ab93431b0b856336710636a
TrizlyBear <tristanschmidt2004@gmail.com>
2021-03-26 09:55:31 +0100

    Ping

--- ayr/handlers/messageCreate.go
+++ ayr/handlers/messageCreate.go
@@ -3,7 +3,7 @@ package handlers
 import "github.com/bwmarrin/discordgo"
 
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate)  {
-	if !m.Author.Bot && m.Content == "ping"{
+	if !m.Author.Bot && m.Content == "Ping!"{
 		s.ChannelMessageSend(m.ChannelID, "Pong! :ping_pong: "+s.HeartbeatLatency().String())
 	}
 
